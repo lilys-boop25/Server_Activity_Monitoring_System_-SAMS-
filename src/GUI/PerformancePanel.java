f5038bc69b23b6770c5714b187d4b3537894c717
tronghieu220403 <tronghieu220403@gmail.com>
2023-06-24 00:42:06 +0700

    Revert "Update code"
    This reverts commit 7652ee56ecb13f438f681ccb069e4ac3ae56199c.

--- src/GUI/PerformancePanel.java
+++ src/GUI/PerformancePanel.java
@@ -102,8 +102,8 @@ public class PerformancePanel extends OshiJPanel{
         GridBagConstraints perfMenuBarConstraints = new GridBagConstraints();
         perfMenuBarConstraints.gridx = 0;
         perfMenuBarConstraints.gridy = 0;
-        //perfMenuBarConstraints.gridwidth = 1d;
-        //perfMenuBarConstraints.gridheight = 1d;
+        perfMenuBarConstraints.weightx = 1d;
+        perfMenuBarConstraints.weighty = 1d;
         perfMenuBarConstraints.anchor = GridBagConstraints.NORTHWEST;
 
         JScrollPane scrollPerfPanel = new JScrollPane(perfMenuBar);
@@ -114,8 +114,6 @@ public class PerformancePanel extends OshiJPanel{
         GridBagConstraints perfConstraints = new GridBagConstraints();
         perfConstraints.gridx = 0;
         perfConstraints.gridy = 0;
-        perfConstraints.gridwidth = 1;
-        perfConstraints.gridheight = 1;
         perfConstraints.weightx = 1d;
         perfConstraints.weighty = 1d;
         perfConstraints.anchor = GridBagConstraints.NORTHWEST;
@@ -136,8 +134,8 @@ public class PerformancePanel extends OshiJPanel{
         GridBagConstraints displayConstraints = new GridBagConstraints();
         displayConstraints.gridx = 1;
         displayConstraints.gridy = 0;
-        displayConstraints.gridwidth = 1;
-        displayConstraints.gridheight = 1;
+        displayConstraints.weightx = 1;
+        displayConstraints.weighty = 1d;
         displayConstraints.fill = GridBagConstraints.NONE;
         displayConstraints.anchor = GridBagConstraints.NORTHWEST;
 
