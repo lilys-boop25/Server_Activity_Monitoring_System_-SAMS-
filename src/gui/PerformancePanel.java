ab457c2abd989f04c252bc29f4eda59c56f73b0f
vuquyen03 <quyenvungoc1711@gmail.com>
2023-07-06 11:44:01 +0700

    fix Code Smell

--- src/gui/PerformancePanel.java
+++ src/gui/PerformancePanel.java
@@ -5,14 +5,14 @@ import javax.swing.*;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.renderer.xy.XYAreaRenderer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import oshi.SystemInfo;
 import oshi.hardware.HWDiskStore;
 import oshi.hardware.NetworkIF;
@@ -79,12 +79,12 @@ public class PerformancePanel extends OshiJPanel{
         JPanel perfMenuBar = new JPanel();
         perfMenuBar.setLayout(new BoxLayout(perfMenuBar, BoxLayout.Y_AXIS));
 
-        JGradientButton cpuButton = createButton("\nCPU\n0%\n", 'C', "Display CPU", Color.GREEN, new CPUPanel(si), displayPanel);
+        JGradientButton cpuButton = createButton("\nCPU\n0%\n", Color.GREEN, new CPUPanel(si), displayPanel);
 
         perfMenuBar.add(cpuButton);
         CPUPanel.updateCPUInfo(si.getHardware().getProcessor(), cpuButton, si.getOperatingSystem());
 
-        JGradientButton memButton = createButton(PerformancePanel.buttonTextLines("\nMemory\n0/0 GB (0%)\n"), 'M', "Display Memory", Color.GREEN,new MemoryPanel(si), displayPanel);
+        JGradientButton memButton = createButton(PerformancePanel.buttonTextLines("\nMemory\n0/0 GB (0%)\n"), Color.GREEN,new MemoryPanel(si), displayPanel);
 
         perfMenuBar.add(memButton);
 
@@ -95,25 +95,25 @@ public class PerformancePanel extends OshiJPanel{
         for (int i = 0; i < hwDiskStore.size() ; i++)
         {
             HWDiskStore disk = hwDiskStore.get(i);
-            diskButton[i] = createButton(DiskPanel.updateDisk(disk, i, 0, 0), 'D', "Display Disk",Color.PINK.darker(), new DiskPanel(disk, i), displayPanel);
+            diskButton[i] = createButton(DiskPanel.updateDisk(disk, i, 0, 0), Color.PINK.darker(), new DiskPanel(disk, i), displayPanel);
             perfMenuBar.add(diskButton[i]);
         }
         DiskPanel.updateDiskInfo(si.getHardware().getDiskStores(), diskButton);
 
-        List<NetworkIF> networkIFs = si.getHardware().getNetworkIFs(true);
+        List<NetworkIF> networkIFs = si.getHardware().getNetworkIFs(false);
 
         JGradientButton[] netButton = new JGradientButton[networkIFs.size()];
         for (int i = 0; i < networkIFs.size() ; i++)
         {
             NetworkIF net = networkIFs.get(i);
-            netButton[i] = createButton(NetworkPanel.updateNetwork(net, 0, 0), 'N', "Display Network",Color.CYAN.brighter() , new NetworkPanel(net, i), displayPanel);
+            netButton[i] = createButton(NetworkPanel.updateNetwork(net, 0, 0),Color.CYAN.brighter() , new NetworkPanel(net, i), displayPanel);
             perfMenuBar.add(netButton[i]);
         }
         NetworkPanel.updateNetWorkInfo(networkIFs, netButton);
 
         JScrollPane scrollPerfPanel = new JScrollPane(perfMenuBar);
         scrollPerfPanel.getVerticalScrollBar().setUnitIncrement(30);
-        scrollPerfPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPerfPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPerfPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
         scrollPerfPanel.setBounds(0, 0, 295, 935);
@@ -152,12 +152,12 @@ public class PerformancePanel extends OshiJPanel{
 
 
 
-    private JGradientButton createButton(String title, char mnemonic, String toolTip, Color color, OshiJPanel panel, JPanel displayPanel)
+    private JGradientButton createButton(String title, Color color, OshiJPanel panel, JPanel displayPanel)
     {
         JGradientButton button = new JGradientButton(title);
         button.color = color;
         button.setFont(button.getFont().deriveFont(16f));
-        button.setHorizontalTextPosition(JButton.LEFT);
+        button.setHorizontalTextPosition(SwingConstants.LEFT);
         button.setHorizontalAlignment(SwingConstants.LEFT);
         // Set what to do when we push the button
         button.addActionListener(e -> {
@@ -182,7 +182,7 @@ public class PerformancePanel extends OshiJPanel{
 
     public static String buttonTextLines(String txt)
     {
-        return "<html>" + htmlSpace(3) + txt.replaceAll("\n", "<br>" + htmlSpace(3));
+        return "<html>" + htmlSpace(3) + txt.replace("\n", "<br>" + htmlSpace(3));
     }
 
     public static String htmlSpace(int num)
